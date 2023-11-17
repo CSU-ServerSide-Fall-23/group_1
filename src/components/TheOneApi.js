929c19e97cc94653c23e231a6f4d6abb26912488
Allen <allencpsc@gmail.com>
2023-11-16 21:04:26 -0500

    moved RecommendedSites functionality to its own file

--- src/components/TheOneApi.js
+++ src/components/TheOneApi.js
@@ -9,8 +9,6 @@ class TheOneApi extends Component{
         }
     }
 
-
-
     componentDidMount(){
         const randomQuoteId = Math.floor(Math.random() *1000)
         const oneApiUrl = 'https://the-one-api.dev/v2/quote'
@@ -21,18 +19,18 @@ class TheOneApi extends Component{
             })
             .then(response=>{
                 this.setState({theOneApiData: response.data.docs[randomQuoteId]})
-                console.log(response);
-                console.log(this.state)
+                //console.log(response);
+                //console.log(this.state)
             })
             .catch(function (error) {
-                // handle error
+                //handle error
                 console.log(error);
             })
     }
 
     render(){
         let quote = this.state.theOneApiData.dialog
-        console.log(this.state.theOneApiData.dialog)
+        //console.log(this.state.theOneApiData.dialog)
         return(
             <footer>
             <React.Fragment>
@@ -43,38 +41,6 @@ class TheOneApi extends Component{
 
     }
 
-
-
 }
 
-    export default TheOneApi
-
-
-// export default function TheOneApi(){
-//     const[oneData, setData] = useState({});
-//     let oneApiUrl = 'https://the-one-api.dev/v2/quote'
-//     const quoteArray = []
-//     axios.get(oneApiUrl, {
-//         headers: {
-//             'Authorization' : 'Bearer ECpzqigl8778NRrj2KKZ'
-//         }
-//         })
-//         .then(response=>{
-//             setData(response.data);
-//             quoteArray = oneData.docs
-//             console.log(response);
-//         })
-//         .catch(function (error) {
-//             // handle error
-//             console.log(error);
-//         })
-//     return(
-//         <div class="container">
-//         <React.Fragment>
-//             red
-//         </React.Fragment>
-//         </div>
-//     )
-// }
-
-//LOTR Quote: {quoteArray[5].dialog}
+export default TheOneApi
