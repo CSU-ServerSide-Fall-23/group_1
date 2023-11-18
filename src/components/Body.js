b2f59ad24ea9c4c7fcf653582efd1091b3b0c4aa
TaoMaru <jackson_maria1@columbusstate.edu>
2023-11-18 17:50:50 -0500

    Enable News widget images; Add Google Search Widget

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,15 +2,21 @@ import News from "./News";
 import Weather from "./Weather";
 import TheOneApi from "./TheOneApi";
 import RecommendedSites from "./RecommendedSites";
+import GoogleSearch from "./GoogleSearch";
 
 
 const Body = () => {
     return(
-        <div class ="container float-start d-flex justify-content-evenly p-3 body-container">
-            <RecommendedSites/>
-            <News />
-            <Weather />
-            <TheOneApi />
+        <div class ="container float-start d-flex flex-column justify-content-evenly p-3 body-container">
+            <div class="d-flex justify-content-center p-3">
+                <GoogleSearch />
+            </div>
+            <div class="d-flex justify-content-evenly p-3">
+                <RecommendedSites/>
+                <News />
+                <Weather />
+                <TheOneApi />
+            </div>
         </div>
     )
 
---
+++ src/components/GoogleSearch.jsx
@@ -0,0 +1,15 @@
+const GoogleSearch = () => {
+    return (
+        <div className="gcse-search">
+            <form className="gsce-searchbox d-flex rounded search-box" role="search">
+              <input className="gsce-input form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
+            </form>
+            <div class="gcse-searchresults"></div>
+
+
+        </div>
+    )
+}
+
+export default GoogleSearch
