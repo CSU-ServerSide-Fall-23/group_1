0cc844584cd937c30f1e0970f8fb12821130435e
Allen <allencpsc@gmail.com>
2023-11-25 17:08:24 -0500

    added cypress tests

--- cypress/e2e/recommendsites.cy.js
+++ cypress/e2e/recommendsites.cy.js
@@ -1,5 +1,71 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('Heading text', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains('Links')
+  })
+
+  it('Wikipedia link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Wikipedia").click()
+  })
+
+  it('Youtube link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Youtube").click()
+  })
+
+  it('GitHub link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("GitHub").click()
+  })
+
+  it('CSU link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Columbus State").click()
+  })
+
+  it('Bootstrap link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Bootstrap Docs").click()
+  })
+
+  it('g4g link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("GeeksforGeeks").click()
+  })
+
+  it('Reddit link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Reddit").click()
+  })
+
+  it('Twitter link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Twitter").click()
+  })
+
+  it('LinkedIn link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("LinkedIn").click()
+  })
+
+  it('OpenAI link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("OpenAI").click()
+  })
+
+  it('Discord link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Discord").click()
+  })
+
+  it('Amazon link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Amazon").click()
+  })
+
+  it('Netflix link', () => {
+    cy.visit('localhost:3000')
+    cy.get('.col').contains("Netflix").click()
   })
 })
